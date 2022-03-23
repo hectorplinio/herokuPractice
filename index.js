52122434d510bc6a0ad699cad032a8e16cf6198f
hectorplinio <hectorplinio@gmail.com>
2022-03-23 12:18:08 +0100

    Push to heroku

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-  return res.status(200).send({
-    message: "<h1>Hello World!</h1>",
-});});
+  return res.status(200).send(
+    "<h1>Hello World!</h1>");});
 
 app.listen(port, () => {
   console.log("SERVER IS LISTENNING IN PORT " + port);
